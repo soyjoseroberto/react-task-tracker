eb96e814459d97c176fdb9ad7368f51a46e44cf1
soyjoseroberto <jrodriguez18@gmail.com>
2021-10-04 22:11:06 -0400

    Added Header component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const name = 'Jose';
+  const isEnabled = true;
   return (
-    <div className="App">
+    <div className="container">
       <h1>Hello From React</h1>
+      <h2>My name is {name}</h2>
+      <h2>Enabled: {isEnabled ? 'Yes' : 'No'}</h2>
+      <Header />
     </div>
   );
 }
