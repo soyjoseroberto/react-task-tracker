39d1753f92d92dfc8525237a0421f5c4a3178da9
soyjoseroberto <jrodriguez18@gmail.com>
2021-10-04 22:17:37 -0400

    Added components folder.

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+// Not needed anymore, just going to comment it out for now
+// import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Task Tracker</h1>
+        </header>
+     )
+}
+
+export default Header
