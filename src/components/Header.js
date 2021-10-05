f9eb887fe8981035787c1065d8b781f7ac741ce4
soyjoseroberto <jrodriguez18@gmail.com>
2021-10-04 22:28:06 -0400

    PropTypes added for type safety.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 
 // Not needed anymore, just going to comment it out for now
 // import React from 'react'
+import PropTypes from 'prop-types' // Type impt to use extension to generate this line
 
-const Header = () => {
+
+const Header = ({title}) => {
     return (
         <header>
             <h1>Task Tracker</h1>
+            <h2>{ title }</h2>
         </header>
      )
+}
+
+Header.defaultProps = {
+    title: 'Jose\'s Task tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
 }
 
 export default Header
