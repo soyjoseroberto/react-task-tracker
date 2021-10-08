bdbd5fb398afc730ec2c2b08a881e05becf6cf41
soyjoseroberto <jrodriguez18@gmail.com>
2021-10-07 20:55:06 -0400

    Added index.css and created Button component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,14 @@
 // Not needed anymore, just going to comment it out for now
 // import React from 'react'
 import PropTypes from 'prop-types' // Type impt to use extension to generate this line
-
+import Button from './Button'
 
 const Header = ({title}) => {
     return (
-        <header>
+        <header className="header">
             <h1>Task Tracker</h1>
-            <h2>{ title }</h2>
+            {/* <h2>{ title }</h2> */}
+            <Button color="black" text="Add" />
         </header>
      )
 }
